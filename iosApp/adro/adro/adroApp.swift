7f26c11f7601f5f225c53dabb1e3025dde0d7dcf
farazconsoliads <faraz.ahmed@consoliads.com>
2022-10-05 00:08:58 +0500

    adding kmm support

---
+++ iosApp/adro/adro/adroApp.swift
@@ -0,0 +1,17 @@
+//
+//  adroApp.swift
+//  adro
+//
+//  Created by Faraz Ahmed on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct adroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
