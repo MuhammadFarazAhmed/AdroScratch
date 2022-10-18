53883798154776ff80fd27873851902b49abc793
farazahmed259 <farazahmed.pk01@gmail.com>
2022-10-18 12:59:04 +0500

    add KMM Implementation

---
+++ iosApp/Adro/Adro/AdroApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdroApp.swift
+//  Adro
+//
+//  Created by Faraz Ahmed on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AdroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iosApp/Adro/Pods/Target Support Files/Pods-Adro-AdroUITests/Pods-Adro-AdroUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Adro_AdroUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Adro_AdroUITests
+@end
