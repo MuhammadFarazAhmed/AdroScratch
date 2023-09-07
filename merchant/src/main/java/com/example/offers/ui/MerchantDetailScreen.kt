1290ea140d424c0638514e1d25f79b88f88e12cd
farazconsoliads <faraz.ahmed@consoliads.com>
2023-09-08 01:27:51 +0500

    local

--- merchant/src/main/java/com/example/offers/ui/MerchantDetailScreen.kt
+++ merchant/src/main/java/com/example/offers/ui/MerchantDetailScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,12 +22,20 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material.icons.outlined.ThumbUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -39,13 +48,13 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -57,6 +66,7 @@ import androidx.constraintlayout.compose.MotionScene
 import androidx.constraintlayout.compose.rememberMotionLayoutState
 import com.example.adro.common.CommonFlowExtensions.collectAsStateLifecycleAware
 import com.example.adro.vm.CommonViewModel
+import com.example.domain.models.MerchantDetailModel
 import com.example.offers.vm.MerchantDetailViewModel
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import org.koin.androidx.compose.getViewModel
@@ -71,6 +81,18 @@ fun MerchantDetailScreen(
     vm: CommonViewModel,
     detailVM: MerchantDetailViewModel = getViewModel()
 ) {
+
+    val lazySections =
+        detailVM.merchantDetailResponse.collectAsStateLifecycleAware().value
+
+    MainLayout(lazySections)
+
+}
+
+@OptIn(ExperimentalMotionApi::class)
+@Composable
+private fun MainLayout(lazySections: List<MerchantDetailModel.Data.Detail>?) {
+
     val maxPx = with(LocalDensity.current) { EXPANDED_TOP_BAR_HEIGHT.roundToPx().toFloat() }
     val minPx = with(LocalDensity.current) { COLLAPSED_TOP_BAR_HEIGHT.roundToPx().toFloat() }
     val toolbarHeight = remember { mutableStateOf(maxPx) }
@@ -99,19 +121,12 @@ fun MerchantDetailScreen(
     }
     val progress = 1 - (toolbarHeight.value - minPx) / (maxPx - minPx)
 
-    Log.d("TAG", "progress: $progress")
-    Log.d("TAG", "maxPx: $maxPx")
-    Log.d("TAG", "minPx: $minPx")
-
-
-    val lazySections =
-        detailVM.merchantDetailResponse.collectAsStateLifecycleAware().value
-
     Surface(
         modifier = Modifier
             .navigationBarsPadding()
             .fillMaxSize()
     ) {
+
         val context = LocalContext.current
         val motionScene = remember {
             context.resources
@@ -119,13 +134,15 @@ fun MerchantDetailScreen(
                 .readBytes()
                 .decodeToString()
         }
+        val motionState = rememberMotionLayoutState()
+        val corners = 10f - ((motionState.currentProgress * 10)).coerceAtMost(10f)
 
         MotionLayout(
             motionScene = MotionScene(content = motionScene),
-            motionLayoutState = rememberMotionLayoutState(),
+            motionLayoutState = motionState,
             modifier = Modifier
-                .background(Color.LightGray)
                 .fillMaxSize()
+                .background(Color.LightGray)
         ) {
 
             Image(
@@ -136,179 +153,124 @@ fun MerchantDetailScreen(
                     .layoutId("headerImage")
             )
 
-            val properties = motionProperties("toolbar")
-
             Box(
                 modifier = Modifier
-//                    .nestedScroll(nestedScrollConnection)
                     .fillMaxHeight()
-                    .background(Color.White)
+                    .background(
+                        Color.White,
+                        shape = RoundedCornerShape(topStart = corners, topEnd = corners)
+                    )
                     .layoutId("contentBg")
-            ) {
-                Text(
-                    text = "You will fall in love with this fresh Strawberry Cake made with a homemade strawberry reduction.\n\n" +
-                            " It's moist, tender, and the most beautiful shade of pink!\n\n" +
-                            "This recipe combines layers of strawberry flavored cake with strawberry flavored cream cheese frosting, so you get fresh strawberry flavors with every bite.\n\n" +
-                            "This made from scratch with strawberry cake recipe is such a delightful and pretty cake. Packed with strawberry flavors and a gorgeous shade of pink, this cake is perfect for any occasion.\n\n" +
-                            "No box mix or Jell-O in this cake! Made with real strawberries, this cake will steal the show with its light and tender crumb\n\n" +
-                            "Filled and coated in a wonderful strawberry cream cheese frosting, this cake has strawberry flavoring through and through.\n\n" +
-                            "Try making my strawberry shortcake recipe, strawberry pie recipe, strawberry chocolate cake recipe if you have extra strawberries on hand.\n" +
-                            "Or, try my red velvet cake recipe or vanilla cake recipe if you want another easy cake recipe. \n" +
-                            "You can leave the strawberry cake at room temperature for up to 5 days if you keep it covered. You can also store the cake in the fridge.\n" +
-                            "\nYou can freeze fully frosted or unfrosted cake for up to 2 months. Wrap the cake tightly in a layer of plastic wrap and then tin foil before freezing.\n\n" +
-                            "Let the cake thaw overnight in the refrigerator, then bring to room temperature before serving.You will fall in love with this fresh Strawberry Cake made with a homemade strawberry reduction.\\n\\n\" +\n" +
-                            "                            \" It's moist, tender, and the most beautiful shade of pink!\\n\\n\" +\n" +
-                            "                            \"This recipe combines layers of strawberry flavored cake with strawberry flavored cream cheese frosting, so you get fresh strawberry flavors with every bite.\\n\\n\" +\n" +
-                            "                            \"This made from scratch with strawberry cake recipe is such a delightful and pretty cake. Packed with strawberry flavors and a gorgeous shade of pink, this cake is perfect for any occasion.\\n\\n\" +\n" +
-                            "                            \"No box mix or Jell-O in this cake! Made with real strawberries, this cake will steal the show with its light and tender crumb\\n\\n\" +\n" +
-                            "                            \"Filled and coated in a wonderful strawberry cream cheese frosting, this cake has strawberry flavoring through and through.\\n\\n\" +\n" +
-                            "                            \"Try making my strawberry shortcake recipe, strawberry pie recipe, strawberry chocolate cake recipe if you have extra strawberries on hand.\\n\" +\n" +
-                            "                            \"Or, try my red velvet cake recipe or vanilla cake recipe if you want another easy cake recipe. \\n\" +\n" +
-                            "                            \"You can leave the strawberry cake at room temperature for up to 5 days if you keep it covered. You can also store the cake in the fridge.\\n\" +\n" +
-                            "                            \"\\nYou can freeze fully frosted or unfrosted cake for up to 2 months. Wrap the cake tightly in a layer of plastic wrap and then tin foil before freezing.\\n\\n\" +\n" +
-                            "                            \"Let the cake thaw overnight in the refrigerator, then bring to room temperature before serving.",
-                    modifier = Modifier.fillMaxHeight()
-                        .layoutId("text")
-                        .padding(horizontal = 16.dp),
-                    fontSize = 14.sp,
-                )
+            )
 
-//                LazyColumn {
-//                    items(DEFAULT_BOOKS) {
-//                        Book(modifier = Modifier, it)
-//                    }
-//                }
+            Text(
+                text = "Fresh Strawberry Cake", fontSize = 22.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.SemiBold, modifier = Modifier
+                    .layoutId("title")
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            )
+
+            Divider(
+                Modifier
+                    .layoutId("titleDivider")
+                    .fillMaxWidth()
+                    .padding(horizontal = 34.dp)
+            )
+
+            Text(
+                text = "by John Kanell", fontSize = 16.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Gray, fontStyle = FontStyle.Italic,
+                modifier = Modifier
+                    .layoutId("subTitle")
+                    .fillMaxWidth()
+                    .padding(6.dp)
+            )
+
+            Divider(
+                Modifier
+                    .layoutId("subTitleDivider")
+                    .fillMaxWidth()
+                    .padding(horizontal = 34.dp)
+            )
+
+            Text(
+                modifier = Modifier
+                    .layoutId("date")
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                text = "September, 2022", fontSize = 16.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Gray
+            )
+
+            val properties = motionProperties("actions")
+
+            Row(
+                modifier = Modifier
+                    .layoutId("actions")
+                    .background(properties.value.color("background")),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                IconButton(onClick = { }) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Icon(Icons.Default.Share, contentDescription = "", tint = Color.White)
+                        Text(text = "SHARE", color = Color.White, fontSize = 12.sp)
+                    }
+                }
+
+                IconButton(onClick = { }) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Icon(Icons.Outlined.ThumbUp, contentDescription = "", tint = Color.White)
+                        Text(text = "LIKE", color = Color.White, fontSize = 12.sp)
+                    }
+                }
+
+                IconButton(onClick = { }) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Icon(Icons.Outlined.Star, contentDescription = "", tint = Color.White)
+                        Text(text = "SAVE", color = Color.White, fontSize = 12.sp)
+                    }
+                }
             }
 
-            TopAppBar(
-                backgroundColor = properties.value.color("background"), modifier = Modifier
-                    .layoutId("toolbar")
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Image(
-                        modifier = Modifier.layoutId("ivBack"),
-                        painter = painterResource(com.example.base.R.drawable.ic_back),
-                        contentDescription = ""
-                    )
-
-                    Image(
-                        modifier = Modifier.layoutId("ivFav"),
-                        painter = painterResource(com.example.base.R.drawable.ic_favorite),
-                        contentDescription = ""
-                    )
+            val books = lazySections?.map { BookModel(it.sectionTitle ?: "", "", 0) }
+            Column(modifier = Modifier) {
+                books?.forEach {
+                    Book(modifier = Modifier, it)
+                } ?: DEFAULT_BOOKS.forEach {
+                    Book(modifier = Modifier, it)
                 }
+            }
+
+            Button(
+                onClick = {
+
+                },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                modifier = Modifier.layoutId("menu"), contentPadding = PaddingValues(4.dp),
+                elevation = ButtonDefaults.elevatedButtonElevation(defaultElevation = 0.dp),
+            ) {
+                Icon(
+                    Icons.Default.ArrowBack,
+                    contentDescription = ""
+                )
             }
 
 
         }
 
     }
-
 }
 
 @OptIn(ExperimentalMotionApi::class)
 @Composable
 @Preview
 private fun ScreenPreview() {
-    val maxPx = with(LocalDensity.current) { EXPANDED_TOP_BAR_HEIGHT.roundToPx().toFloat() }
-    val minPx = with(LocalDensity.current) { COLLAPSED_TOP_BAR_HEIGHT.roundToPx().toFloat() }
-    val toolbarHeight = remember { mutableStateOf(maxPx) }
-
-    val nestedScrollConnection = remember {
-        object : NestedScrollConnection {
-            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
-                val height = toolbarHeight.value
-
-                if (height + available.y > maxPx) {
-                    toolbarHeight.value = maxPx
-                    return Offset(0f, maxPx - height)
-                }
-
-                if (height + available.y < minPx) {
-                    toolbarHeight.value = minPx
-                    return Offset(0f, minPx - height)
-                }
-
-                toolbarHeight.value += available.y
-                return Offset(0f, available.y)
-            }
-
-        }
-    }
-    val progress = 1 - (toolbarHeight.value - minPx) / (maxPx - minPx)
-
-    Surface(
-        modifier = Modifier
-            .navigationBarsPadding()
-            .fillMaxSize()
-    ) {
-        val context = LocalContext.current
-        val motionScene = remember {
-            context.resources
-                .openRawResource(com.example.base.R.raw.motion_scene)
-                .readBytes()
-                .decodeToString()
-        }
-
-        MotionLayout(
-            progress = progress,
-            motionScene = MotionScene(content = motionScene),
-            modifier = Modifier
-                .background(Color.LightGray)
-                .fillMaxSize()
-        ) {
-
-            Image(
-                painter = painterResource(id = com.example.base.R.drawable.demoimage),
-                contentDescription = "",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .layoutId("headerImage")
-            )
-
-            Row(
-                modifier = Modifier
-                    .layoutId("toolbar")
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Image(
-                    modifier = Modifier.layoutId("ivBack"),
-                    painter = painterResource(com.example.base.R.drawable.ic_back),
-                    contentDescription = ""
-                )
-
-                Image(
-                    modifier = Modifier.layoutId("ivFav"),
-                    painter = painterResource(com.example.base.R.drawable.ic_favorite),
-                    contentDescription = ""
-                )
-            }
-
-            Box(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .nestedScroll(nestedScrollConnection)
-                    .background(Color.White)
-                    .layoutId("contentBg")
-            ) {
-
-                LazyColumn {
-                    items(DEFAULT_BOOKS) {
-                        Book(modifier = Modifier, it)
-                    }
-                }
-            }
-
-
-        }
-
-    }
+    MainLayout(null)
 }
 
 @Composable
